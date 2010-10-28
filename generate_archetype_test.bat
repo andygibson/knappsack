a173ccfb9ad5f09a5d99e3ef02371f8a3e28a6a1
Andy Gibson <contact@andygibson.net>
2010-10-28 21:31:03 +0000

    Added local repo lines.

--- generate_archetype_test.bat
+++ generate_archetype_test.bat
@@ -1,20 +1,33 @@
-#batch file used to generate all archetypes 
-set repo = -Dmaven.repo.remote=https://oss.sonatype.org/content/repositories/orgfluttercode-545/
-set version=1.0.5
+rem #DON't RUN IN KNAPPSACK PROJECT FOLDER!
+rem #
+rem #
+rem #batch file used to generate all archetypes 
+rem #
+rem #
+rem #
+rem # Set repo below
+rem #
+rem #set repo = -Dmaven.repo.remote=https://oss.sonatype.org/content/repositories/orgfluttercode-545/
 
-call mvn archetype:generate -DarchetypeGroupId=org.fluttercode.knappsack -DarchetypeArtifactId=jee6-basic-archetype  -DinteractiveMode=false -DarchetypeVersion=%version% -DgroupId=org.application -DartifactId=jee6basic -Dpackage=org.application  %repo%
+rem #
+rem #
+rem #
+set version=1.0.6-SNAPSHOT
+set repo=file://C:/temp/maven2repo/
 
-call mvn archetype:generate -DarchetypeGroupId=org.fluttercode.knappsack -DarchetypeArtifactId=jee6-minimal-archetype -DinteractiveMode=false -DarchetypeVersion=%version% -DgroupId=org.application -DartifactId=jee6minimal -Dpackage=org.application %repo%
+call mvn archetype:generate -DarchetypeGroupId=org.fluttercode.knappsack -DarchetypeArtifactId=jee6-basic-archetype  -DinteractiveMode=false -DarchetypeVersion=%version% -DgroupId=org.application -DartifactId=jee6basic -Dpackage=org.application -DarchetypeRepository=%repo%
 
-call mvn archetype:generate -DarchetypeGroupId=org.fluttercode.knappsack -DarchetypeArtifactId=jee6-sandbox-archetype -DinteractiveMode=false -DarchetypeVersion=%version% -DgroupId=org.application -DartifactId=jee6sandbox -Dpackage=org.application %repo%
+call mvn archetype:generate -DarchetypeGroupId=org.fluttercode.knappsack -DarchetypeArtifactId=jee6-minimal-archetype -DinteractiveMode=false -DarchetypeVersion=%version% -DgroupId=org.application -DartifactId=jee6minimal -Dpackage=org.application -DarchetypeRepository=%repo%
 
-call mvn archetype:generate -DarchetypeGroupId=org.fluttercode.knappsack -DarchetypeArtifactId=jee6-sandbox-demo-archetype -DinteractiveMode=false -DarchetypeVersion=%version% -DgroupId=org.application -DartifactId=jee6sboxdemo -Dpackage=org.application %repo%
+call mvn archetype:generate -DarchetypeGroupId=org.fluttercode.knappsack -DarchetypeArtifactId=jee6-sandbox-archetype -DinteractiveMode=false -DarchetypeVersion=%version% -DgroupId=org.application -DartifactId=jee6sandbox -Dpackage=org.application -DarchetypeRepository=%repo%
 
-call mvn archetype:generate -DarchetypeGroupId=org.fluttercode.knappsack -DarchetypeArtifactId=jee6-servlet-basic-archetype -DinteractiveMode=false -DarchetypeVersion=%version% -DgroupId=org.application -DartifactId=servletbasic -Dpackage=org.application %repo%
+call mvn archetype:generate -DarchetypeGroupId=org.fluttercode.knappsack -DarchetypeArtifactId=jee6-sandbox-demo-archetype -DinteractiveMode=false -DarchetypeVersion=%version% -DgroupId=org.application -DartifactId=jee6sboxdemo -Dpackage=org.application -DarchetypeRepository=%repo%
 
-call mvn archetype:generate -DarchetypeGroupId=org.fluttercode.knappsack -DarchetypeArtifactId=jee6-servlet-minimal-archetype -DinteractiveMode=false -DarchetypeVersion=%version% -DgroupId=org.application -DartifactId=servletminimal -Dpackage=org.application %repo%
+call mvn archetype:generate -DarchetypeGroupId=org.fluttercode.knappsack -DarchetypeArtifactId=jee6-servlet-basic-archetype -DinteractiveMode=false -DarchetypeVersion=%version% -DgroupId=org.application -DartifactId=servletbasic -Dpackage=org.application -DarchetypeRepository=%repo%
 
-call mvn archetype:generate -DarchetypeGroupId=org.fluttercode.knappsack -DarchetypeArtifactId=jee6-servlet-sandbox-archetype -DinteractiveMode=false -DarchetypeVersion=%version% -DgroupId=org.application -DartifactId=servletsandbox -Dpackage=org.application %repo%
+call mvn archetype:generate -DarchetypeGroupId=org.fluttercode.knappsack -DarchetypeArtifactId=jee6-servlet-minimal-archetype -DinteractiveMode=false -DarchetypeVersion=%version% -DgroupId=org.application -DartifactId=servletminimal -Dpackage=org.application -DarchetypeRepository=%repo%
+
+call mvn archetype:generate -DarchetypeGroupId=org.fluttercode.knappsack -DarchetypeArtifactId=jee6-servlet-sandbox-archetype -DinteractiveMode=false -DarchetypeVersion=%version% -DgroupId=org.application -DartifactId=servletsandbox -Dpackage=org.application -DarchetypeRepository=%repo%
 
 
-call mvn archetype:generate -DarchetypeGroupId=org.fluttercode.knappsack -DarchetypeArtifactId=jee6-servlet-demo-archetype -DinteractiveMode=false -DarchetypeVersion=%version% -DgroupId=org.application -DartifactId=servletdemol -Dpackage=org.application %repo%
+call mvn archetype:generate -DarchetypeGroupId=org.fluttercode.knappsack -DarchetypeArtifactId=jee6-servlet-demo-archetype -DinteractiveMode=false -DarchetypeVersion=%version% -DgroupId=org.application -DartifactId=servletdemol -Dpackage=org.application -DarchetypeRepository=%repo%
